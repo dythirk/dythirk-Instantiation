b7dda5bd145cb38425fa994be4bce985c7b01420
Thai Vo <dythirk@gmail.com>
2024-08-06 08:16:38 +0000

    Instantiation

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        int[] array = new int[3]; 
+        return array;
     }
 }
